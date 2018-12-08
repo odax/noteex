35ba8b5c32750748973284b8465bd0f50dc55971
odax <nr10@albion.edu>
2018-12-08 10:31:31 -0500

    rendered landing page

---
+++ client/src/components/LandingPage/IntBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+//IntBox is short for InteractionBox
+const IntBox = () => {
+    return (
+      <div>
+        Landing Page
+      </div>
+    )
+};
+
+export default IntBox;
---
+++ client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import IntBox from './IntBox';
+
+const LandingPage = () => {
+    return (
+      <div className = 'LandingPage'>
+        <IntBox/>
+      </div>
+    )
+};
+
+export default LandingPage;
