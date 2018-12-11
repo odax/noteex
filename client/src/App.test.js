6bdc7fdabc42d0ca3943ddad887f2763cc7a0ffa
odax <nr10@albion.edu>
2018-12-11 07:11:42 -0500

    added enzyme, configured app test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AppContextProvider } from './AppContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Router>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </Router>
+    ,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
