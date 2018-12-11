5a2d92ba4093ef137fb0ca0aa76bade240d03346
odax <nr10@albion.edu>
2018-12-11 03:49:54 -0500

    added some dependencies

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        Logged In
+      </div>
+    )
+  }
+}
